b6c9e71e4e73cdae5d4002d9d8a0516edb36413b
rxh020301 <rxh020301@darden.com>
2023-11-22 15:23:24 +0530

    demo commit

---
+++ DemoGithub/src/Demo/Demogithub.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class Demogithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("this is task 1 of today's task");
+	}
+
+}
---
+++ DemoGithub/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author richandr
+ *
+ */
+module DemoGithub {
+}
